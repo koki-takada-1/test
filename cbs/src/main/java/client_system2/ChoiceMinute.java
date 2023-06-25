01552d92afdc30b034ebc9234d42632748fffe43
koki-takada-1 <koukigaotian39@gmail.com>
2023-06-25 17:37:54 +0900

    first commit

---
+++ cbs/src/main/java/client_system2/ChoiceMinute.java
@@ -0,0 +1,13 @@
+package client_system2;
+
+import java.awt.*;
+
+public class ChoiceMinute extends Choice{
+	//コンストラクタ
+	public ChoiceMinute() {
+		add("00");
+		add("15");
+		add("30");
+		add("45");
+	}
+}
